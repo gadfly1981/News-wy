d6b7ffb31f77671b63002d464f356d8611069109
gadfly1981 <gadfly1981@gmail.com>
2023-11-09 00:51:47 +0800

    Initial Commit

---
+++ News-wy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  News-wy
+//
+//  Created by wang yu on 2023/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ News-wy/News_wyApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_wyApp.swift
+//  News-wy
+//
+//  Created by wang yu on 2023/11/9.
+//
+
+import SwiftUI
+
+@main
+struct News_wyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
